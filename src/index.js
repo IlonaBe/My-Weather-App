59e35da9f2d41113235c592a876b973348c89b23
Ilona Berzina <80828932+IlonaBe@users.noreply.github.com>
2021-03-21 19:31:50 +0100

    added hours forecast

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-let now = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-let dateAndTime = document.querySelector(".card-text");
-
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = "0" + minutes;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
 }
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let day = days[now.getDay()];
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
+  return `${hours}:${minutes}`;
+}
 let months = [
   "Jan",
   "Feb",
@@ -34,11 +41,13 @@ let months = [
   "Nov",
   "Dec"
 ];
-let month = months[now.getMonth()];
 
-dateAndTime.innerHTML = `${day} ${date}. ${month}, ${hours}:${minutes}`;
+
+
 
 function displayWeatherCondition(response) {
+  let dateAndTime = document.querySelector("#date");
+  dateAndTime.innerHTML = formatDate(response.data.dt * 1000);
   let weatherNowIcon = document.querySelector("#weatherNowIcon");
   document.querySelector("h1").innerHTML = response.data.name;
   document.querySelector(".weather-now p").innerHTML = Math.round(
@@ -58,12 +67,44 @@ function displayWeatherCondition(response) {
   console.log(response);
 }
 
+function displayForecast(response){
+  let forecastHours = document.querySelector("#hours-forecast");
+  forecastHours.innerHTML = null;
+  let forecast = null;
+  console.log(forecast);
+
+  for (let index = 0; index < 4; index++) {
+    forecast = response.data.list[index];
+        forecastHours.innerHTML += `
+        
+          <div class="row row-cols-3">                                    
+              <div class="col text-end">
+                 ${formatHours(forecast.dt * 1000)}
+              </div>
+              <div class="col text-end">
+              ${Math.round(forecast.main.temp)}°C
+               </div>
+              <div class="col text-start" id="hour-icon">
+                <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="SunCloudSnow" width="40px" />
+              </div>
+            </div>
+         
+      `;  
+    }
+  }
+
+
+
+
 function searchCity(city) {
   let units = "metric";
   let apiKey = "09b98a8b6fbfa8f93e206c9bfb83f786";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -77,6 +118,7 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
+
 }
 
 
